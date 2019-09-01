c9cc3990c49a90336553ed134cce6cc89f5a14bf
Daria <dariaburdyga@gmail.com>
2019-09-01 21:41:10 +0300

    added new name for helloworld

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/store';
 import Buefy from 'buefy';
 import './store/interceptors/loading';
 import 'bulma/css/bulma.css';
---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import loading from './loading';
+import posts from './posts';
+import alertState from '../common/vue-telekom/components/Alert/alertStore';
+import fileUploadState from '../common/vue-telekom/components/FileUploader/fileUploadStore';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {
+    alertState,
+    fileUploadState,
+    loading,
+    posts
+  }
+});
+
+
