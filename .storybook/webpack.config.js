ad997de71d69e675b687a9b82d4a759c56be4214
Daria <dariaburdyga@gmail.com>
2019-09-06 12:18:35 +0300

    added styles for Errormessage

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,46 @@
-const path = require('path');
+// const path = require('path');
+//
+// // Export a function. Accept the base config as the only param.
+// module.exports = async ({ config, mode }) => {
+//   // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+//   // You can change the configuration based on that.
+//   // 'PRODUCTION' is used when building the static version of storybook.
+//
+//   // Make whatever fine-grained changes you need
+//   config.module.rules.push({
+//     test: /\.scss$/,
+//     use: ['style-loader', 'css-loader', 'sass-loader'],
+//     include: path.resolve(__dirname, '../'),
+//   });
+//
+//   // Return the altered config
+//   return config;
+// };
 
-// Export a function. Accept the base config as the only param.
-module.exports = async ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
 
-  // Make whatever fine-grained changes you need
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: ['style-loader', 'css-loader', 'sass-loader'],
-    include: path.resolve(__dirname, '../'),
-  });
+const path = require("path");
 
-  // Return the altered config
-  return config;
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              resources: [
+                "src/common/vue-telekom/styles/base/variables.scss",
+                "src/common/vue-telekom/styles/utilities/mixins.scss",
+              ]
+            },
+          },
+        ],
+        include: path.resolve(__dirname, "../")
+      }
+    ]
+  }
 };
